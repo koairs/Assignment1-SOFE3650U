82e3f3d7a70b3c515f133f196755d3e6174c3911
koairs <kkaahh452@gmail.com>
2025-09-22 19:39:53 -0400

    Add files via upload

---
+++ sourceFiles/BrandBBulb.java
@@ -0,0 +1,19 @@
+public class BrandBBulb implements Bulb{
+    private int powerUsage;
+
+    public void setPowerUsage(int powerUsage) {
+        this.powerUsage = powerUsage;
+    }
+
+    public int getPowerUsage() {
+        return powerUsage;
+    }
+
+    public void turnOn() {
+        System.out.println("This Brand B bulb is turned now on. Using "+ powerUsage + " watts.");
+    }
+
+    public void turnOff() {
+        System.out.println("This Brand B bulb is turned off.");
+    }
+}
